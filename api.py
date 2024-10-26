dd0f9e62dea752a3bd3027467a07bace986b255b
Eyad Yasser Shaban <eyadshaban99@gmail.com>
2024-10-26 14:47:06 +0300

    fine tuning to model

--- api.py
+++ api.py
@@ -38,21 +38,57 @@ class Portrait(Resource):
         image = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
         
         if image is None:
-            abort(400, message="Invalid image format")
+            abort(400, message="No Image Uploaded")
         
-        # Convert the image to base64
-        _, image_encoded = cv2.imencode('.png', image)
+        
+        face_cascade = cv2.CascadeClassifier(   #good lightwieght face detector for portrait detection
+            cv2.data.haarcascades + 'haarcascade_frontalface_default.xml'
+        )
+        
+        # Convert to grayscale for the model
+        grayScale = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        
+        # Detect faces
+        faces = face_cascade.detectMultiScale(
+            grayScale,
+            scaleFactor = 1.05, # is acceptable given speed is not an issue for this model
+            minNeighbors = 8, # given aim of API false negatives are more detrimental than false positives
+            minSize = (25, 25)
+        )
+        
+        if len(faces) == 0:
+            abort(400, error="No faces detected in the image")
+        
+        #if len(faces) > 1:
+        #    abort(400, error="Multiple faces detected in the image")
+        
+        # Get the first face detected
+        x, y, w, h = faces[0]
+        
+        # Add some padding around the face to get full portrait
+        padding = int(max(w, h) * 0.25)
+        x = max(0, x - padding)
+        y = max(0, y - padding)
+        w = min(w + 2*padding, image.shape[1] - x)
+        h = min(h + 2*padding, image.shape[0] - y)
+        
+        # Extract the face region
+        face_image = image[y:y+h, x:x+w]
+                
+        
+        _, image_encoded = cv2.imencode('.png', face_image)
         image_base64 = base64.b64encode(image_encoded).decode('utf-8')
         
-        # Ensure directory exists
-        save_folder = 'rawdata'
-        if not os.path.exists(save_folder):
-            os.makedirs(save_folder)
-        
-        # Save the base64 image to a file
-        with open(os.path.join(save_folder, 'img'), 'w') as image_file:
-            image_file.write(image_base64)
-        
+        # save b64 for debug purposes
+        if(app.debug):
+            save_folder = 'rawdata'
+            if not os.path.exists(save_folder):
+                os.makedirs(save_folder)
+            
+            # Save the base64 image to a file
+            with open(os.path.join(save_folder, 'img.out'), 'w') as image_file:
+                image_file.write(image_base64)
+            
         return {"image": image_base64}
 
 api.add_resource(Portrait, "/portrait_extract/")
